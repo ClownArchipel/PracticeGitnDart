c08b6dfdbe06b6f9bf3b90328769d1c3ffa46881
ClownArchipel <yanuade2000@gmail.com>
2023-11-07 15:49:42 +0700

    First Commit

---
+++ bin/console_app_practice.dart
@@ -0,0 +1,17 @@
+import 'package:console_app_practice/console_app_practice.dart' as console_app_practice;
+
+void main(List<String> arguments) {
+  print('mantap');
+  double jajan ;
+  jajan = 2.2;
+  print(jajan);
+  print('Hello world: ${console_app_practice.calculate()}!');
+  integer();
+}
+
+integer(){
+  var integer = 0;
+  integer = 2;
+  print(integer);
+  return integer;
+}
---
+++ test/console_app_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_app_practice/console_app_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
