538532cf1712a80a1fceddcec2752e894c5d6fb8
ClownArchipel <yanuade2000@gmail.com>
2023-11-08 11:16:31 +0700

    mntap

--- bin/console_app_practice.dart
+++ bin/console_app_practice.dart
@@ -5,8 +5,13 @@ import '../folder_cabang/hallocabang.dart';
 void main(List<String> arguments) {
   print('mantap');
   double jajan ;
-  jajan = 2.2;
-  print(jajan);
+  jajan = 109.08;
+  int pk = 1;
+  num j = 1;
+  num k = 20.09;
+  var h = j+k;
+  var ol = pk+jajan;
+  print('j = $j, k = $k, ol = ${ol.runtimeType}, dan terakhir h = $h');
   print('Hello world: ${console_app_practice.calculate()}!');
   integer();
   toPrint();
