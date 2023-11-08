79f4e31ed10416c52e06d29eb74bd37c557dc47e
ClownArchipel <yanuade2000@gmail.com>
2023-11-08 09:23:48 +0700

    updated sikit

--- bin/console_app_practice.dart
+++ bin/console_app_practice.dart
@@ -1,4 +1,5 @@
 import 'package:console_app_practice/console_app_practice.dart' as console_app_practice;
+import '../folder_cabang/hallocabang.dart';
 
 void main(List<String> arguments) {
   print('mantap');
